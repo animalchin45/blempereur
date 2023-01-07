31cb580eb6612731318e63138fb599c73167474a
Tim O'Brien <timobrien.inc@gmail.com>
2023-01-07 15:34:02 -0500

    updated npm modules, email address, and title

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -3,44 +3,42 @@ import { Link } from 'react-router-dom'
 import Testimonial from './Testimonial'
 import { useMediaQuery } from 'react-responsive'
 
-import {
-    barbaraSquare,
-    orchidHero,
-    orchidHeroMobile
-} from '../../img/index'
+import { barbaraSquare, orchidHero, orchidHeroMobile } from '../../img/index'
 
 const Hero = () => {
-    const isMobile = useMediaQuery({ query: '(max-width: 1200px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 1200px)' })
 
-    return (
-        <section 
-            className="hero"
-            style={
-                {
-                    backgroundImage: `url(${isMobile ? orchidHeroMobile : orchidHero})`
-                }
-            }
-        >
-            <div className="hero__logo">
-                <img className="hero__logo--img" src={barbaraSquare} alt="helping"/>
-                <div className="hero__logo--title">
-                    <h1>Barbara Lempereur</h1>
-                    <h2>Unified Therapist & Counselor</h2>
-                </div>
-            </div>
-            <div className="hero__welcome">
-                <h3 className="hero__welcome--brand">Unified Therapy &trade;<br/>Where the Mind Meets the Body</h3>
-                <h4>305-401-2914</h4>
-                <a href="mailto:barbara@blempereur.com">
-                    <h4>barbara@blempereur.com</h4>
-                </a>
-                <Link to="/newclients">
-                    <button className="btn">Start Now</button>
-                </Link>
-            </div>
-            <Testimonial />
-        </section>
-    )
+  return (
+    <section
+      className='hero'
+      style={{
+        backgroundImage: `url(${isMobile ? orchidHeroMobile : orchidHero})`,
+      }}
+    >
+      <div className='hero__logo'>
+        <img className='hero__logo--img' src={barbaraSquare} alt='helping' />
+        <div className='hero__logo--title'>
+          <h1>Barbara Lempereur</h1>
+          <h2>Unified Therapist</h2>
+        </div>
+      </div>
+      <div className='hero__welcome'>
+        <h3 className='hero__welcome--brand'>
+          Unified Therapy &trade;
+          <br />
+          Where the Mind Meets the Body
+        </h3>
+        <h4>305-401-2914</h4>
+        <a href='mailto:barblemp32@gmail.com'>
+          <h4>barblemp32@gmail.com</h4>
+        </a>
+        <Link to='/newclients'>
+          <button className='btn'>Start Now</button>
+        </Link>
+      </div>
+      <Testimonial />
+    </section>
+  )
 }
 
 export default Hero
--- src/components/new-clients/Questions.js
+++ src/components/new-clients/Questions.js
@@ -4,28 +4,24 @@ import faqs from './faqs'
 import { v4 as uuidv4 } from 'uuid'
 
 const Questions = () => {
-    const renderedFaqs = faqs.map((faq) => {
-        return (
-            <Collapsible 
-                transitionTime={200}
-                trigger={faq.title}
-                key={uuidv4()}
-            >
-                <p>{faq.text}</p>
-            </Collapsible>
-        )
-    })
-
+  const renderedFaqs = faqs.map((faq) => {
     return (
-        <section className="questions">
-            <div className="questions__content">
-                <h3>Fequently Asked Questions</h3>
-                <div className="questions__collapsible">
-                    {renderedFaqs}
-                </div>
-            </div>
-        </section>
+      <div key={uuidv4()}>
+        <Collapsible key={uuidv4()} transitionTime={200} trigger={faq.title}>
+          <p key={uuidv4()}>{faq.text}</p>
+        </Collapsible>
+      </div>
     )
+  })
+
+  return (
+    <section className='questions'>
+      <div className='questions__content'>
+        <h3>Fequently Asked Questions</h3>
+        <div className='questions__collapsible'>{renderedFaqs}</div>
+      </div>
+    </section>
+  )
 }
 
 export default Questions
