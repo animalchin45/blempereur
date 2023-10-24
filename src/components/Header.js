9ffd5cc26b0b3d0e8c87a4146aacd0298fa2fe64
Tim O'Brien <timobrien.inc@gmail.com>
2023-10-24 10:26:18 -0400

    Remove "counseler" from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import { Link, NavLink, useLocation } from 'react-router-dom'
-import { flowerHeader } from '../img/index'
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { flowerHeader } from '../img/index';
 
 const Header = () => {
   // Header logo and background states
-  const [headerLogo, setHeaderLogo] = useState(false)
-  const [headerBackgroundColor, setHeaderBackgroundColor] = useState(false)
+  const [headerLogo, setHeaderLogo] = useState(false);
+  const [headerBackgroundColor, setHeaderBackgroundColor] = useState(false);
   // Page location variable
-  const location = useLocation()
+  const location = useLocation();
   // Settings for on / off states of header background
   const colorOff = {
     backgroundImage:
       'linear-gradient(90deg, rgba(173,214,225,0) 25%, rgba(173,214,225,0) 100%)',
     backgroundSize: 'cover',
     backgroundPosition: 'right center',
-  }
+  };
   const colorOn = {
     boxShadow: '0 1px 2px rgba(13, 20, 9, 1)',
     backgroundImage: `linear-gradient(90deg, rgba(173,214,225,1) 25%, rgba(173,214,225,0) 100%), url(${flowerHeader})`,
     backgroundSize: 'cover',
     backgroundPosition: 'right center',
-  }
+  };
 
   useEffect(() => {
     // When page scroll position is further than 350 pixles apply changes to header
     if (location.pathname === '/') {
       function showHeaderLogo() {
         if (window.scrollY >= 350) {
-          setHeaderLogo(true)
-          setHeaderBackgroundColor(true)
+          setHeaderLogo(true);
+          setHeaderBackgroundColor(true);
         } else {
-          setHeaderLogo(false)
-          setHeaderBackgroundColor(false)
+          setHeaderLogo(false);
+          setHeaderBackgroundColor(false);
         }
       }
-      setHeaderLogo(false)
-      setHeaderBackgroundColor(false)
-      window.addEventListener('scroll', showHeaderLogo, true)
+      setHeaderLogo(false);
+      setHeaderBackgroundColor(false);
+      window.addEventListener('scroll', showHeaderLogo, true);
       return () => {
-        window.removeEventListener('scroll', showHeaderLogo, true)
-      }
+        window.removeEventListener('scroll', showHeaderLogo, true);
+      };
     } else {
-      setHeaderLogo(true)
-      setHeaderBackgroundColor(true)
+      setHeaderLogo(true);
+      setHeaderBackgroundColor(true);
     }
-  }, [location.pathname])
+  }, [location.pathname]);
 
   return (
     <div className='header' style={headerBackgroundColor ? colorOn : colorOff}>
@@ -81,7 +81,7 @@ const Header = () => {
         </NavLink>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
